1fc21b69c2393a04340687abc371a896014e49f5
Syifa Ulyanida <syifaulya0610@gmail.com>
2023-03-12 21:40:18 +0700

    add pages and init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Navb from "./components/Navb";
+import Detail from "./pages/Detail";
+import Favorites from "./pages/Favorites";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Navb />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/detail" element={<Detail />} />
+          <Route path="/favorite" element={<Favorites />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
